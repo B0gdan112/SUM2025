801995efea5bfc74e4441a996d6b1012f1ca6013
Сангаджиев Богдан (BS7) <cgsg112@ad.pml30.ru>
2025-06-24 11:59:52 +0300

    Add T09ANIM project.

---
+++ T09ANIM/bin/shaders/font3d/frag.glsl
@@ -0,0 +1,19 @@
+/* Fragment shader by BS7 */
+
+#version 460
+out vec4 OutColor;
+ 
+in vec4 DrawColor;
+in vec2 DrawTexCoord;
+ 
+layout(binding = 0) uniform sampler2D Tex;
+uniform bool IsTexture0;
+ 
+void main( void )
+{
+  vec4 tc = texture(Tex, DrawTexCoord);
+ 
+  if (tc.a < 0.30)
+    discard;
+  OutColor = vec4(tc.rgb, tc.a);
+}
